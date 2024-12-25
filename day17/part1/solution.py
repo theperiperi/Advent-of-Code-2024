6ffc60e4338a678d81abc5ba10323bffe0da2bce
periperi <priyasridhar101@gmail.com>
2024-12-25 11:53:41 +0530

    day 17

---
+++ day17/part1/solution.py
@@ -0,0 +1,72 @@
+def parse_input(filename):
+    # Read and parse input file into program instructions
+    with open(filename) as fname:
+        lines = fname.readlines()
+        initial_a = int(lines[0].split(": ")[1])
+        initial_b = int(lines[1].split(": ")[1])
+        initial_c = int(lines[2].split(": ")[1])
+        program = [int(x) for x in lines[4].split(": ")[1].split(",")]
+        return initial_a, initial_b, initial_c, program
+
+def part1(initial_a, initial_b, initial_c, program):
+    # Solve part 1: Run program with given initial values
+    # Returns comma-separated string of outputs
+    
+    # Registers
+    A = initial_a
+    B = initial_b
+    C = initial_c
+    
+    # Program Counter
+    ip = 0
+    
+    # Output storage
+    output = []
+    
+    def combo_value(operand):
+        # Helper function to get the value of a combo operand
+        if operand in range(4):  # Literal values 0-3
+            return operand
+        elif operand == 4:  # Value of register A
+            return A
+        elif operand == 5:  # Value of register B
+            return B
+        elif operand == 6:  # Value of register C
+            return C
+        else:
+            raise ValueError("Invalid operand: 7 encountered")
+    
+    # Execute the program
+    while ip < len(program):
+        opcode = program[ip]
+        operand = program[ip + 1]
+        
+        if opcode == 0:  # adv: A = A // (2 ** combo_value)
+            A = A // (2 ** combo_value(operand))
+        elif opcode == 1:  # bxl: B = B ^ operand
+            B = B ^ operand
+        elif opcode == 2:  # bst: B = combo_value % 8
+            B = combo_value(operand) % 8
+        elif opcode == 3:  # jnz: if A != 0, jump to operand
+            if A != 0:
+                ip = operand
+                continue
+        elif opcode == 4:  # bxc: B = B ^ C
+            B = B ^ C
+        elif opcode == 5:  # out: output combo_value % 8
+            output.append(combo_value(operand) % 8)
+        elif opcode == 6:  # bdv: B = A // (2 ** combo_value)
+            B = A // (2 ** combo_value(operand))
+        elif opcode == 7:  # cdv: C = A // (2 ** combo_value)
+            C = A // (2 ** combo_value(operand))
+        else:
+            raise ValueError(f"Invalid opcode: {opcode}")
+        
+        ip += 2
+    
+    return ",".join(map(str, output))
+
+# Read input and solve puzzle
+initial_a, initial_b, initial_c, program = parse_input('day17/input.txt')
+print(part1(initial_a, initial_b, initial_c, program))
+
---
+++ day17/part2/solution.py
@@ -0,0 +1,113 @@
+import os
+
+# Define the required functions if util is not available
+def getlines(day):
+    # Read lines from 'input.txt' located in the 'day17' folder
+    input_path = os.path.join(day, 'input.txt')  # Assuming your 'day17' folder is in the current directory
+    with open(input_path, 'r') as file:
+        return file.read().splitlines()
+
+def tokenedlines(line):
+    # Placeholder for the actual implementation
+    return line.split()
+
+def Tuple(a, b):
+    # Placeholder for the actual implementation
+    return (a, b)
+
+day = "day17"  # Folder name
+
+# Read lines from 'input.txt' inside the 'day17' folder
+lines = getlines(day)
+
+# Processing the data (same as you had)
+def getdata(line):
+    # Check if the line contains the expected format
+    if ": " in line:
+        return line.split(": ")[1]
+    return None  # Return None or handle the error as needed
+
+# Extracting the values
+a = int(getdata(lines[0]))  # Ensure lines[0] is valid
+b = int(getdata(lines[1]))  # Ensure lines[1] is valid
+c = int(getdata(lines[2]))  # Ensure lines[2] is valid
+program = [int(x) for x in getdata(lines[4]).split(",")] if getdata(lines[4]) else []
+
+# Function definitions (same as you had)
+def combo(a, b, c, value):
+    if value < 4:
+        return value
+    if value == 4:
+        return a
+    if value == 5:
+        return b
+    if value == 6:
+        return c
+    return None
+
+def eval(a, b, c, ip, program):
+    opcode = program[ip]
+    arg = program[ip+1]
+    comb = combo(a, b, c, arg)
+    if opcode == 0:
+        num = a
+        denom = pow(2, comb)
+        return (None, num // denom, b, c, ip + 2)
+    elif opcode == 1:
+        return (None, a, b ^ arg, c, ip + 2)
+    elif opcode == 2:
+        return (None, a, comb % 8, c, ip + 2)
+    elif opcode == 3:
+        if a == 0:
+            return (None, a, b, c, ip + 2)
+        else:
+            return (None, a, b, c, arg)
+    elif opcode == 4:
+        return (None, a, b ^ c, c, ip + 2)
+    elif opcode == 5:
+        return (comb % 8, a, b, c, ip + 2)
+    elif opcode == 6:
+        num = a
+        denom = pow(2, comb)
+        return (None, a, num // denom, c, ip + 2)
+    elif opcode == 7:
+        num = a
+        denom = pow(2, comb)
+        return (None, a, b, num // denom, ip + 2)
+
+def run_program(a, b, c, program):
+    ip = 0
+    res = []
+    while ip < len(program) - 1:
+        (out, a, b, c, ip) = eval(a, b, c, ip, program)
+        if out is not None:
+            res.append(out)
+    return res
+
+def get_best_quine_input(program, cursor, sofar):
+    for candidate in range(8):
+        if run_program(sofar * 8 + candidate, 0, 0, program) == program[cursor:]:
+            if cursor == 0:
+                return sofar * 8 + candidate
+            ret = get_best_quine_input(program, cursor - 1, sofar * 8 + candidate)
+            if ret is not None:
+                return ret
+    return None
+
+def parse_input(filename):
+    # Read and parse input file into program instructions
+    with open(filename) as fname:
+        lines = fname.readlines()
+        initial_a = int(lines[0].split(": ")[1])
+        initial_b = int(lines[1].split(": ")[1])
+        initial_c = int(lines[2].split(": ")[1])
+        program = [int(x) for x in lines[4].split(": ")[1].split(",")]
+        return initial_a, initial_b, initial_c, program
+
+def part2(program):
+    # Find quine input that generates program as output
+    return get_best_quine_input(program, len(program) - 1, 0)
+
+# Read input and solve puzzle
+_, _, _, program = parse_input('day17/input.txt')
+print(part2(program))
